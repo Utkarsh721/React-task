2e4660c8cb38d77db47b90ffb2f9e0fc99a3d0de
Utkarsh721 <utkarshrai2121>
2023-01-21 19:51:45 +0530

    React task 9

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import ExpanseItem from './Component/ExpenseItem';
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: 'Mumbai',
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), location: 'Delhi' },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: 'Chennai',
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: 'Manali'
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {expenses.map((item, key) => (
+    <div key={key} className='bigContainer'>
+       <ExpanseItem title={item.title} amount={item.amount}  date={item.date} location={item.location}/>
+    
+     </div>
+))}
+      
+       {/* <ExpanseItem 
+      title={expenses[1].title} 
+      amount={expenses[1].amount}
+      date={expenses[1].date}
+      location={expenses[1].location}
+      ></ExpanseItem>
+       <ExpanseItem 
+      title={expenses[2].title} 
+      amount={expenses[2].amount}
+      date={expenses[2].date}
+      location={expenses[2].location}
+      ></ExpanseItem>
+       <ExpanseItem 
+      title={expenses[3].title} 
+      amount={expenses[3].amount}
+      date={expenses[3].date}
+      location={expenses[3].location}
+      ></ExpanseItem> */}
+       {/* title={expenses[0].title} 
+      amount={expenses[0].amount}
+      date={expenses[0].date}
+      location={expenses[0].location} */}
+      
     </div>
   );
 }
