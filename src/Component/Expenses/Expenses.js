460ad1b86b57ccd6f4c934c9435ebaddb16e84f9
Utkarsh721 <utkarshrai2121>
2023-01-27 01:15:21 +0530

    Advance React tasj 14

--- src/Component/Expenses/Expenses.js
+++ src/Component/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 import Card from "../UI/Card";
-import './Expenses.css';
+
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
-
+import ExpensesChart from "./ExpensesChart";
+import './Expenses.css';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021');
 
@@ -23,6 +24,7 @@ const Expenses = (props) => {
          selected={filteredYear} 
          onChangeFilter={filterChangeHandler} 
          />
+         <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
 
       </Card>
